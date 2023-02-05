c32280353faf8acfcdb8e29ecc096e499f991431
InfoyTech <90406016+PSGitHubUser1@users.noreply.github.com>
2023-02-05 18:51:03 +0530

    Add files via upload

---
+++ Install_Alt_Run-As-Admin.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+cd C:\Windows\Cursors
+mkdir Win11_Dark_HD_Cursors
+
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\alternate.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\beam.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\busy.ani C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\dgn1.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\dgn2.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\handwriting.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\help.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\horz.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\link.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\move.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\person.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\pin.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\pointer.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\precision.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\unavailable.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\vert.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\working.ani C:\Windows\Cursors\Win11_Dark_HD_Cursors
+
+pause
