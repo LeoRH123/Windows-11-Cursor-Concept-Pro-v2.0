e2854cdb30f894fd968d326973ca28c9ecf84cbf
InfoyTech <90406016+PSGitHubUser1@users.noreply.github.com>
2023-02-05 19:08:46 +0530

    Update Install_Alt_Run-As-Admin.cmd

--- Install_Alt_Run-As-Admin.cmd
+++ Install_Alt_Run-As-Admin.cmd
@@ -6,21 +6,37 @@ mkdir Win11_Dark_HD_Cursors
 REM Change the "rksra" to your User Profile like "JohnTim" or take %USERPROFILE% or %HOMEDRIVE%%HOMEPATH% 
 
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\alternate.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\beam.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\busy.ani C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\dgn1.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\dgn2.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\handwriting.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\help.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\horz.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\link.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\move.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\person.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\pin.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\pointer.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\precision.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\unavailable.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\vert.cur C:\Windows\Cursors\Win11_Dark_HD_Cursors
+echo.
 copy C:\Users\rksra\Desktop\Windows.Cursor.Concept.v2.2\dark\cursor\working.ani C:\Windows\Cursors\Win11_Dark_HD_Cursors
-
+echo.
 pause
